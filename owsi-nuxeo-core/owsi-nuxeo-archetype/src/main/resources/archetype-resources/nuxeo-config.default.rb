ade37f3f054befdc2e2051e61380baafe341f91c
avinot <antoine.vinot@openwide.fr>
2016-05-17 16:29:19 +0200

    Déplacement Nuxeo Archetype

---
+++ owsi-nuxeo-core/owsi-nuxeo-archetype/src/main/resources/archetype-resources/nuxeo-config.default.rb
@@ -0,0 +1,6 @@
+# Target custom Nuxeo
+#NUXEO_PATH = '/home/username/nuxeo-cap-${nuxeoVersion}-tomcat/'
+
+# Distrib config
+MARKETPLACE_PACKAGE_NAME = '${artifactId}'
+MARKETPLACE_PROJECT_PATH = './${artifactId}-distribution'
